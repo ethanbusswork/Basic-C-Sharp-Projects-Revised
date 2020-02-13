119c64acf9f5872edccd27e3ab04f1fded6706f8
Ethan B <ethanbuss.work@gmail.com>
2020-02-12 16:28:40 -0800

    Completed StringsAndStringBuilder

---
+++ StringsAndStringbuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndStringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string word1 = "Hello ";
+            //string word2 = "Ethan, ";
+            //string word3 = "welcome.";
+            //Console.WriteLine(word1 + word2 + word3);
+            //Console.Read();
+
+            //string upperCase = "uppercase method";
+            //Console.WriteLine(upperCase.ToUpper());
+            //Console.Read();
+
+            //StringBuilder paragraph = new StringBuilder();
+
+            //paragraph.Append("\tLorem ipsum dolor sit amet, consectetur adipiscing elit. ");
+            //paragraph.Append("\nNulla fermentum velit ante, vitae ullamcorper nibh interdum ac. ");
+            //paragraph.Append("\nPhasellus commodo varius sapien, in imperdiet lacus suscipit sed. ");
+            //paragraph.Append("\nNullam in velit elit. ");
+            //paragraph.Append("Nunc accumsan odio eu massa iaculis, ac ru\ntrum libero pharetra.");
+
+            //Console.WriteLine(paragraph);
+            //Console.Read();
+        }
+    }
+}
