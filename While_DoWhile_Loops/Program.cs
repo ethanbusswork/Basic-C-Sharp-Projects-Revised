857764270455b5e7cfdfb4af645001fc92d96d8a
Ethan B <ethanbuss.work@gmail.com>
2020-02-12 16:04:32 -0800

    Completed DO WHILE statement assignment

---
+++ While_DoWhile_Loops/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_DoWhile_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a color:");
+            string guessedColor = Console.ReadLine();
+            bool guessedCorrect = false;
+
+            //while (!guessedCorrect)
+            //{
+            //    switch (guessedColor)
+            //    {
+            //        case "Red":
+            //            Console.WriteLine("You guessed Red, that is incorrect, try again");
+            //            Console.WriteLine("Guess a color:");
+            //            guessedColor = Console.ReadLine();
+            //            break;
+
+            //        case "Orange":
+            //            Console.WriteLine("You guessed Orange, that is correct!");
+            //            guessedCorrect = true;
+            //            break;
+
+            //        case "Green":
+            //            Console.WriteLine("You guessed Green, that is incorrect, try again");
+            //            Console.WriteLine("Guess a color:");
+            //            guessedColor = Console.ReadLine();
+            //            break;
+
+            //        case "Blue":
+            //            Console.WriteLine("You guessed Blue, that is incorrect, try again");
+            //            Console.WriteLine("Guess a color:");
+            //            guessedColor = Console.ReadLine();
+            //            break;
+
+            //        case "Purple":
+            //            Console.WriteLine("You guessed Purple, that is incorrect, try again");
+            //            Console.WriteLine("Guess a color:");
+            //            guessedColor = Console.ReadLine();
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("That's not a valid color, try again");
+            //            Console.WriteLine("Guess a color:");
+            //            guessedColor = Console.ReadLine();
+            //            break;
+            //    }
+            //}
+            //Console.Read();
+
+
+            do
+            {
+                switch (guessedColor)
+                {
+                    case "Red":
+                        Console.WriteLine("You guessed Red, that is incorrect, try again");
+                        Console.WriteLine("Guess a color:");
+                        guessedColor = Console.ReadLine();
+                        break;
+
+                    case "Orange":
+                        Console.WriteLine("You guessed Orange, that is correct!");
+                        guessedCorrect = true;
+                        break;
+
+                    case "Green":
+                        Console.WriteLine("You guessed Green, that is incorrect, try again");
+                        Console.WriteLine("Guess a color:");
+                        guessedColor = Console.ReadLine();
+                        break;
+
+                    case "Blue":
+                        Console.WriteLine("You guessed Blue, that is incorrect, try again");
+                        Console.WriteLine("Guess a color:");
+                        guessedColor = Console.ReadLine();
+                        break;
+
+                    case "Purple":
+                        Console.WriteLine("You guessed Purple, that is incorrect, try again");
+                        Console.WriteLine("Guess a color:");
+                        guessedColor = Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("That's not a valid color, try again");
+                        Console.WriteLine("Guess a color:");
+                        guessedColor = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!guessedCorrect);
+            
+            Console.Read();
+        }
+    }
+}
