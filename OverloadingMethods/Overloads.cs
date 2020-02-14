a013f9acae469f1516e6a0c969c5ae6aa1ab4406
Ethan B <ethanbuss.work@gmail.com>
2020-02-14 15:33:23 -0800

    Completed OverloadingMethods

---
+++ OverloadingMethods/Overloads.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    public class Overloads
+    {
+        public int Multiply(int num1)
+        {
+            int result;
+
+            result = num1 * 2;
+
+            return result;
+        }
+
+        public decimal Multiply(decimal num1)
+        {
+            decimal result;
+
+            result = num1 * 2;
+
+            return result;
+        }
+        public string Multiply(string num1)
+        {
+            int result;
+
+            int num1_converted = Convert.ToInt32(num1);
+
+            result = num1_converted * 2;
+
+            return result.ToString();
+        }
+    }
+}
---
+++ OverloadingMethods/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[1] Multiply an integer method");
+            Console.WriteLine("[2] Multiply a decimal method");
+            Console.WriteLine("[3] Multiply a string method");
+            Console.WriteLine("\nSelect Math Operation:");
+            int menuSelect = Convert.ToInt32(Console.ReadLine());
+
+            if (menuSelect == 1)
+            {
+                Overloads n = new Overloads();
+
+                Console.WriteLine("\nSELECTED: Multiply an integer method");
+
+                Console.WriteLine("\nEnter a whole number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nMultiplying " + num1 + " by 2");
+                int ret = n.Multiply(num1);
+                Console.WriteLine("Your answer is: " + ret);
+            }
+            else if (menuSelect == 2)
+            {
+                Overloads n = new Overloads();
+
+                Console.WriteLine("\nSELECTED: Multiply a decimal method");
+
+                Console.WriteLine("\nEnter a whole number:");
+                decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("\nMultiplying " + num1 + " by 2");
+                decimal ret = n.Multiply(num1);
+                Console.WriteLine("Your answer is: " + ret);
+            }
+            else if (menuSelect == 3)
+            {
+                Overloads n = new Overloads();
+
+                Console.WriteLine("\nSELECTED: Multiply a string method");
+
+                Console.WriteLine("\nEnter a whole number:");
+                string num1 = Console.ReadLine();
+
+                Console.WriteLine("\nMultiplying " + num1 + " by 2");
+                string ret = n.Multiply(num1);
+                Console.WriteLine("Your answer is: " + ret);
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid menu item");
+            }
+
+            Console.Read();
+        }
+    }
+}
