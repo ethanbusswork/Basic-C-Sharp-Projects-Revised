c9aaab365f7f132c6c147a00d23fa8bdf0d0f50f
Ethan B <ethanbuss.work@gmail.com>
2020-02-13 16:15:44 -0800

    Completed TryCatchAssignment

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number:");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a number:");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine("Your divided number is: " + numberThree);
+                Console.Read();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ TryCatchAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> intList = new List<int>{ 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+
+                Console.WriteLine("Pick a number from 1 - 9:");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < intList.Count; i++)
+                {
+                    Console.WriteLine("Dividing " + intList[i] + " by " + input + ". The answer is: " + (intList[i] / input));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
