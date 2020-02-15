7611c9054e4dd687ec940eb1d33b87e407581f30
Ethan B <ethanbuss.work@gmail.com>
2020-02-15 13:26:26 -0800

    Stuck on optional argument

---
+++ OptionalInputs/OptionalInputs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInputs
+{
+    class OptionalInputs
+    {
+        public int Mult(int num1, int num2 = 2)
+        {
+            int result;
+
+            result = num1 * num2;
+
+            return result;
+        }
+    }
+}
---
+++ OptionalInputs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalInputs n = new OptionalInputs();
+
+            Console.WriteLine("<<<<<<<<<< OPTIONAL INPUTS APP >>>>>>>>>>");
+            Console.WriteLine("\nYou have the option of entering two inputs.");
+            Console.WriteLine("The first input however, is mandatory.");
+            Console.WriteLine("\nFirst input:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second input (Default: 2):");
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int ret = n.Mult(num1, num2);
+
+            Console.WriteLine("Multiplying...");
+            Console.WriteLine("Your answer is: " + ret);
+
+            Console.Read();
+        }
+        class OptionalInputs
+        {
+            public int Mult(int num1, int num2 = 2)
+            {
+                int result;
+
+                result = num1 * num2;
+
+                return result;
+            }
+        }
+    }
+}
