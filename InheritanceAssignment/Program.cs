a1a21f12a9c41914691ad4d98e6478b51e35d929
Ethan B <ethanbuss.work@gmail.com>
2020-02-17 17:53:16 -0800

    Completed InheritanceAssignment

---
+++ InheritanceAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee p = new Employee() { FirstName = "Ethan", LastName = "Buss" };
+            p.SayName();
+        }
+    }
+}
