846a3aa89322b35d6c17afad4b24ee4b68dbdc0c
Ethan B <ethanbuss.work@gmail.com>
2020-02-18 19:44:41 -0800

    Completed StructAssignment

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 2.5m;
+
+            Console.WriteLine(num1.Amount);
+
+            Console.Read();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+
+        }
+    }
+}
