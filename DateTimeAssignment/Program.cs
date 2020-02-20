7ae1b30ca41b27f0418989051f9ee250b8f80369
Ethan B <ethanbuss.work@gmail.com>
2020-02-19 19:25:50 -0800

    Completed DateTimeAssignment

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = new DateTime();
+            DateTime addedHours = new DateTime();
+            Console.WriteLine(DateTime.Now + "\n");
+
+            Console.WriteLine("Please enter an integer");
+            double addHour = Convert.ToInt32(Console.ReadLine());
+
+            rightNow = DateTime.Now;
+
+            addedHours = rightNow.AddHours(addHour);
+
+            Console.WriteLine("The time in {0} hours is {1}", addHour, addedHours);
+
+            Console.Read();
+        }
+    }
+}
--- TwentyOne/Dealer.cs
+++ TwentyOne/Dealer.cs
@@ -21,6 +21,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\ethan\Documents\GitHub\Basic-C-Sharp-Projects-Revised\TwentyOne\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
