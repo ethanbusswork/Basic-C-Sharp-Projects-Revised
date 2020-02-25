9160c2453e8371a56d40723f1ec5f3f4ee8cc9b4
Ethan B <ethanbuss.work@gmail.com>
2020-02-24 17:36:47 -0800

    Completed Assignment_231
    Created Separate dll for TwentyOne

---
+++ Assignment_231/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string string1 = "Ethan";
+            var string2 = "Buss";
+        }
+    }
+
+    public class SampleClass
+    {
+        public SampleClass(): this(10)
+        {
+
+        }
+        public SampleClass(int num1)
+        {
+            
+        }
+    }
+}
--- Casino/Dealer.cs
+++ Casino/Dealer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-namespace TwentyOne
+namespace Casino
 {
     public class Dealer
     {
