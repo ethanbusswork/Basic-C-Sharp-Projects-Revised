6e62e7408d070518df78829ec2465fc73db2cf4c
Ethan B <ethanbuss.work@gmail.com>
2020-02-29 09:16:16 -0800

    Completed CodeFirst DB

---
+++ Scores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\n Welcome back, {uName}. Today is {date}. ";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\ethan\Documents\GitHub\Basic-C-Sharp-Projects-Revised\Scores\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent Scores: ");
+            foreach (string line in lines)
+            {
+                Console.Write("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\n\nAverage score: " + avgScore);
+
+
+            Console.Read();
+        }
+    }
+}
